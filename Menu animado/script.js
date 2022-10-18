7e8bd4406a06c54d6b33bc90a02b1536b0a867e6
Giovanni de Campos Tatiyama <104636002+GiovanniTatiyama@users.noreply.github.com>
2022-10-18 11:16:04 -0300

    Delete Menu animado directory

--- Menu animado/script.js
+++
@@ -1,5 +0,0 @@
-function menuanimado() {
-    document.getElementById('menu').classList.toggle('change')
-    document.getElementById('nav').classList.toggle('change')
-    document.getElementById('menu-bg').classList.toggle('change-bg')
-}
